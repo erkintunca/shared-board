03e7f23161a9496af6d2da4f39f6750592f6974a
Erkin Tunca <erkin.tunca@gmail.com>
2025-02-11 02:18:27 +0300

    -

--- client/script.js
+++ client/script.js
@@ -1,20 +1,17 @@
 const board = document.getElementById('board');
 const addPostItButton = document.getElementById('add-post-it'); // Select the button element
-/*
-var API_URL = 'http://localhost:3000';
-//try {API_URL = import.meta.resolve("/");} catch (error) {console.warn('Assumed to run locally.', error);}
-try {API_URL = import.meta.env.API_URL;} catch (error) {console.warn('Assumed to run locally.', error);}
-console.log(API_URL);
-*/
 
-const API_URL = process.env.API_URL;
+var API_URL = 'http://localhost:3000';
+try {API_URL = import.meta.resolve("/");} catch (error) {console.warn('Assumed to run locally.', error);}
+//try {API_URL = import.meta.env.API_URL;} catch (error) {console.warn('Assumed to run locally.', error);}
+if (API_URL.startsWith("https://127.0.0.1:")) {API_URL = "https://shared-board-y6cp.onrender.com";}
 console.log(API_URL);
 
 async function fetchPostItNotes() {
   try {
     //const response = await fetch('http://localhost:3000/post-its'); // Fetch from your backend API
     //const response = await fetch('https://shared-board-y6cp.onrender.com/post-its'); // Fetch from your backend API
-    const response = await fetch(`${{API_URL}}/post-its`);
+    const response = await fetch(`${API_URL}/post-its`);
     const postItNotes = await response.json();
 
     const postItList = document.createElement('ul'); // Create an unordered list element
