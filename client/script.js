0d13da47e8f2b45a3cea3575bbae676ed225da17
Erkin Tunca <erkin.tunca@gmail.com>
2025-02-11 01:43:51 +0300

    -

--- client/script.js
+++ client/script.js
@@ -3,7 +3,7 @@ const addPostItButton = document.getElementById('add-post-it'); // Select the bu
 
 var API_URL = 'http://localhost:3000';
 //try {API_URL = import.meta.resolve("/");} catch (error) {console.warn('Assumed to run locally.', error);}
-try {API_URL = import.meta.end.API_URL;} catch (error) {console.warn('Assumed to run locally.', error);}
+try {API_URL = import.meta.env.API_URL;} catch (error) {console.warn('Assumed to run locally.', error);}
 console.log(API_URL);
 
 async function fetchPostItNotes() {
