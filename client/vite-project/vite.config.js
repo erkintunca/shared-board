049eada79be935d4d81298af922ac068b7ac65a2
Erkin Tunca <erkin.tunca@gmail.com>
2025-02-11 15:12:13 +0300

    -

--- client/vite-project/vite.config.js
+++ client/vite-project/vite.config.js
@@ -3,11 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '../',
   server: {
-    cors: {
-			origin: ['https://shared-board-client.onrender.com', 'http://localhost:5173'],
-			methods: ['GET', 'POST'],
-			allowedHeaders: ['Content-Type']
-		},
-    allowedHosts: ['https://shared-board-client.onrender.com']  // Add your Render domain
+    allowedHosts: true, 
   }
 });
