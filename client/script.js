16786ccde6242d2679e94807a1a5fa808a09033f
Erkin Tunca <erkin.tunca@gmail.com>
2025-02-11 13:23:39 +0300

    -

--- client/script.js
+++ client/script.js
@@ -1,17 +1,11 @@
 const board = document.getElementById('board');
 const addPostItButton = document.getElementById('add-post-it'); // Select the button element
 
-var API_URL = 'http://localhost:3000';
-try {API_URL = import.meta.resolve("/");} catch (error) {console.warn('Assumed to run locally.', error);}
-//try {API_URL = import.meta.env.API_URL;} catch (error) {console.warn('Assumed to run locally.', error);}
-if (API_URL.startsWith("https://shared-board-client.onrender.com")) {API_URL = "https://shared-board-y6cp.onrender.com";}
-console.log(API_URL);
-
 async function fetchPostItNotes() {
   try {
     //const response = await fetch('http://localhost:3000/post-its'); // Fetch from your backend API
     //const response = await fetch('https://shared-board-y6cp.onrender.com/post-its'); // Fetch from your backend API
-    const response = await fetch(`${API_URL}/post-its`);
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/post-its`);
     const postItNotes = await response.json();
 
     const postItList = document.createElement('ul'); // Create an unordered list element
@@ -37,7 +31,7 @@ async function addPostIt() {
     };
 
     //const response = await fetch('https://shared-board-y6cp.onrender.com/post-its', { // Replace with your backend URL
-    const response = await fetch(`${API_URL}/post-its`, { // Replace with your backend URL
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/post-its`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(newPostIt),
