375036509beb383ef299056ddada9451b1b13730
Erkin Tunca <erkin.tunca@gmail.com>
2025-02-11 15:20:55 +0300

    -

--- client/vite-project/vite.config.js
+++ client/vite-project/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '../',
   server: {
+    host: true, // Ensure the server listens on all addresses
     allowedHosts: [
       'https://shared-board-client.onrender.com', 
       'https://shared-board-y6cp.onrender.com']  // Add your Render domain
