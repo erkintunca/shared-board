f18950112541f0409936af51c5615ebd0c1a85e1
Erkin Tunca <erkin.tunca@gmail.com>
2025-02-11 15:22:47 +0300

    -

--- client/vite-project/vite.config.js
+++ client/vite-project/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     host: true, // Ensure the server listens on all addresses
     allowedHosts: [
-      'https://shared-board-client.onrender.com', 
-      'https://shared-board-y6cp.onrender.com']  // Add your Render domain
+      'shared-board-client.onrender.com', 
+      'shared-board-y6cp.onrender.com']  // Add your Render domain
   }
 });
